cd5025690879549d606dff70439a46e44bb2e3e2
ivan <ivansyn2013@yandex.ru>
2023-05-08 18:14:15 +0300

    dz6 labiriont parse

---
+++ lesson6/labirint_books/labirint_books/settings.py
@@ -0,0 +1,94 @@
+# Scrapy settings for labirint_books project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "labirint_books"
+
+SPIDER_MODULES = ["labirint_books.spiders"]
+NEWSPIDER_MODULE = "labirint_books.spiders"
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+#CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 1
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 5
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = True
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+   "Accept-Language": "en-US,en;q=0.5",
+    "cookie":"PHPSESSID=dgh0ooepls9d0pj7s9uo0upkbl; id_post=182386; UserSes=labdgh0ooepls9d0pj; br_webp=67; begintimed=MTY4MzU1NzczNg%3D%3D; st_uid=38409888795e342569b53b1e4249f2e8; _ga_21PJ900698=GS1.1.1683557759.1.1.1683557894.0.0.0; _ga=GA1.2.1270956872.1683557760; _ga_283NG2S1HR=GS1.1.1683557760.1.1.1683557893.0.0.0; _gid=GA1.2.133101633.1683557766; tmr_lvid=1b8504900a496af9f05ea48dfe3b8c8e; tmr_lvidTS=1683557767604; _ym_uid=1683557769127192895; _ym_d=1683557769; _ym_visorc=b; _ym_isad=1; tmr_detect=0%7C1683557899184; adrdel=1; adrcid=Akn63nWsu4SyuCEqEOy28mA; cookie_policy=1; fotorama-item-226793=0"
+}
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    "labirint_books.middlewares.LabirintBooksSpiderMiddleware": 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    "labirint_books.middlewares.LabirintBooksDownloaderMiddleware": 543,
+#}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   "labirint_books.pipelines.LabirintBooksPipeline": 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
